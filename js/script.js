08e1d111f2843df59b695156d2549f5670f86120
Luca Giozza <luca.giozza@gmail.com>
2021-06-08 16:17:20 +0200

    continuazione esercizio

--- js/script.js
+++ js/script.js
@@ -43,25 +43,34 @@ console.log(numeri)
 // In seguito deve chiedere all’utente (100 - 16) volte di
 //  inserire un numero alla volta, sempre compreso tra 1 e 100.
 
-// var inserisci = parseInt(prompt('inserisci un numero da 1 a 100'));
-
-//  while( inserisci < 1 && inserisci > 100){
-//      var inserisci = parseint(prompt('attenzione, il numero deve essere compreso tra 1 e 100'))
-//  }
 
 
  for(var i= 0 ; i < 84; i++  ){
 
     var inserisci = parseInt(prompt('inserisci un numero da 1 a 100'));
        if(inserisci < 1 || inserisci > 100 ){
-           alert('non inserire numeri minori di 0 o maggiori di 100')
+           alert('non inserire numeri minori di 0 o maggiori di 100');
        }
+      
     
     if( inserisci != numeri[i]){
       
     }else if( inserisci == numeri[i]){
-        alert('hai preso la bomba,hai perso')
+        alert('hai preso la bomba,hai perso');
     }
 
  }
+
+// L’utente non può inserire più volte lo stesso numero.
+
+// non so come si faccia
+
+
+
+
+// Se il numero è presente nella lista dei numeri generati,
+// la partita termina, altrimenti si continua chiedendo all’utente
+//  un altro numero. La partita termina quando il giocatore inserisce
+// un numero “vietato” o raggiunge il numero massimo possibile 
+// di numeri consentiti.
 
