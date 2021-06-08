d9fad9d909e0efdd9c2d2280a57158c4b1dceb71
Luca Giozza <luca.giozza@gmail.com>
2021-06-08 17:34:03 +0200

    prova non riuscita con includes

--- js/script.js
+++ js/script.js
@@ -42,35 +42,43 @@ console.log(numeri)
 
 // In seguito deve chiedere all’utente (100 - 16) volte di
 //  inserire un numero alla volta, sempre compreso tra 1 e 100.
-
-
-
- for(var i= 0 ; i < 84; i++  ){
-
-    var inserisci = parseInt(prompt('inserisci un numero da 1 a 100'));
-       if(inserisci < 1 || inserisci > 100 ){
-           alert('non inserire numeri minori di 0 o maggiori di 100');
-       }
-      
-    
-    if( inserisci != numeri[i]){
-      
-    }else if( inserisci == numeri[i]){
-        alert('hai preso la bomba,hai perso');
-    }
-
- }
-
-// L’utente non può inserire più volte lo stesso numero.
-
-// non so come si faccia
-
-
-
-
 // Se il numero è presente nella lista dei numeri generati,
 // la partita termina, altrimenti si continua chiedendo all’utente
 //  un altro numero. La partita termina quando il giocatore inserisce
 // un numero “vietato” o raggiunge il numero massimo possibile 
-// di numeri consentiti.
+// di numeri consentiti
+
+
+
+  for(var i= 0 ; i < 84; i++  ){
+
+     var inserisci = parseInt(prompt('inserisci un numero da 1 a 100'));
+         if(inserisci < 1 || inserisci > 100 ){
+             alert('non inserire numeri minori di 0 o maggiori di 100');
+         }
+         
+         
+    }
+      
+         var numeroRandom = numeroCasuale(1,100);
+      if( inserisci != numeri[i]){
+          var inserisci = parseInt(prompt('inserisci un numero da 1 a 100'));
+      }else if( !numeri.includes(numeroRandom)){
+          numeri.push(numeroRandom);
+
+          alert('hai preso la bomba,hai perso');
+          break
+     }
+
+  }
+ 
+
+//  L’utente non può inserire più volte lo stesso numero.
+
+//  non so come si faccia
+
+
+
+
+
 
