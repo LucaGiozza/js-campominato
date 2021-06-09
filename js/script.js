4c8e984625336a9e7fb2589877cdd147207026ef
Luca Giozza <luca.giozza@gmail.com>
2021-06-09 16:30:40 +0200

    continuazone esercizio

--- js/script.js
+++ js/script.js
@@ -25,8 +25,8 @@ while(numeri.length < 16 ){
  if(!numeri.includes(numeroRandom)){
      numeri.push(numeroRandom);
  }
-
 }
+
 
 function numeroCasuale(min,max){
     return Math.floor(Math.random() * (max - min + 1) + min);
@@ -37,9 +37,26 @@ function numeroCasuale(min,max){
 console.log(numeri)
 
 
+function presenteInArray(array, element) {
+    var i = 0;
+    var result = false;
+    while (i < array.length && result == false) {
+      if (array[i] == element) {
+        result = true;
+      }
+      i++;
+    }
+    return result;
+  }
 
-
-
+// while (numeriRandom.length < 16) {
+//     //inserisco solo se il numero non è già presente nell'array
+//     var numeroCasuale = generaNumeriRandom(1, 100);
+//     var cerca = presenteInArray(numeriRandom, numeroCasuale);
+//     if (cerca == false) {
+//       numeriRandom.push(numeroCasuale);
+//     }
+//   }
 
 // In seguito deve chiedere all’utente (100 - 16) volte di
 //  inserire un numero alla volta, sempre compreso tra 1 e 100.
@@ -49,35 +66,42 @@ console.log(numeri)
 // un numero “vietato” o raggiunge il numero massimo possibile 
 // di numeri consentiti
 
-var semaforo = true
+var semaforo = true;
 var numeriGiusti = []
+var inserimento = numeriGiusti.push(inserisci)
+var sconfitta = document.getElementById("risposta").innerHtml = 'Hai perso, hai beccato la bomba'
 
-   while(sefamoro ){
+   while(semaforo ){
         var inserisci = parseInt(prompt('inserisci un numero da 1 a 100'));
 
 
-    if ( numeri.includes(inserisci) {
+    if ( numeri.includes(inserisci)) {
           
-
+           sconfitta
            alert('hai preso la bomba,hai perso');
 
-          semaforo = false
+          semaforo = false;
+         
       }else{
-          numeriGiusti.push(inserisci)
+          inserimento
       }
-     
+
+      if(inserisci < 1 || inserisci > 100){
+          alert('attenzione,inserire solo umeri compresi tra zero e 100')
+      }
+    
 
    }
 
+  
+
 
  
 
 
  
 
-//  L’utente non può inserire più volte lo stesso numero.
 
-// 
 
 
 
